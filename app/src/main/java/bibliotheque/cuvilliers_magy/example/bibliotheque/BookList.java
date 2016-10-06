ce95631ead5e0746025ca83f150de52f20fbce66
Magy Alessandro <magy.alessandro@gmail.com>
2016-10-06 16:41:23 +0200

    Get books by DATABASE

--- app/src/main/java/bibliotheque/cuvilliers_magy/example/bibliotheque/BookList.java
+++ app/src/main/java/bibliotheque/cuvilliers_magy/example/bibliotheque/BookList.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import bibliotheque.cuvilliers_magy.example.bibliotheque.database.MySQLiteHelper;
 import bibliotheque.cuvilliers_magy.example.bibliotheque.model.Book;
 import bibliotheque.cuvilliers_magy.example.bibliotheque.model.BookCollection;
 
@@ -20,23 +21,27 @@ public class BookList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_livres);
 
+        MySQLiteHelper dbhelper = new MySQLiteHelper(this);
+
         ListView bookList = (ListView) findViewById(R.id.booklist);
 
         BookCollection books = new BookCollection();
-        List<Map<String, String>> listOfBook = new ArrayList<Map<String, String>>();
+        List<Map<String, String>> listOfBook; //= new ArrayList<Map<String, String>>();
 
-        books.add(new Book("Jimmy","There is no need to be upset","823"));
+        /*books.add(new Book("Jimmy","There is no need to be upset","823"));
         books.add(new Book("***","Mon combat contre l'alcoolisme","476"));
-        books.add(new Book("JUL","ça fait 5 ans que j'nique le game","259"));
+        books.add(new Book("JUL","ça fait 5 ans que j'nique le game","259"));*/
 
-        for( Book book : books.getBooks()) {
+        /*for( Book book : books.getBooks()) {
             Map<String, String> bookMap = new HashMap<String, String>();
             bookMap.put("img", String.valueOf(R.mipmap.ic_launcher)); // use available img
             bookMap.put("author", book.getAuthor());
             bookMap.put("title", book.getTitle());
             bookMap.put("isbn", book.getIsbn());
             listOfBook.add(bookMap);
-        }
+        }*/
+
+        listOfBook = dbhelper.getAllBooks();
 
         SimpleAdapter listAdapter = new SimpleAdapter(this.getBaseContext(), listOfBook, R.layout.book_detail,
                 new String[] {"img", "author", "title", "isbn"},
---
+++ app/src/main/java/bibliotheque/cuvilliers_magy/example/bibliotheque/database/MySQLiteHelper.java
@@ -0,0 +1,82 @@
+package bibliotheque.cuvilliers_magy.example.bibliotheque.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import bibliotheque.cuvilliers_magy.example.bibliotheque.R;
+import bibliotheque.cuvilliers_magy.example.bibliotheque.model.Book;
+import bibliotheque.cuvilliers_magy.example.bibliotheque.model.BookCollection;
+
+/**
+ * Created by magy on 06/10/16.
+ */
+
+public class MySQLiteHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "livres.db";
+    public static final int DATABASE_VERSION = 1;
+    public static SQLiteDatabase database;
+    public static final String DATABASE_CREATE = "" +
+            "CREATE TABLE livre (id integer primary key, titre VARCHAR(30), auteur VARCHAR(30));\n";
+
+    public static final String DATABASE_INSERT = "" +
+            "INSERT INTO livre VALUES (259, \"JUL\", \"ça fait 5 ans que j'nique le game\");\n";
+
+    public MySQLiteHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        database = super.getReadableDatabase();
+        //database.execSQL(DATABASE_INSERT);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public List<Map<String, String>> getAllBooks(){
+        String[] columns = new String[3];
+        columns[0] = "id";
+        columns[1] = "titre";
+        columns[2] = "auteur";
+
+        Cursor cursor = database.query("livre", columns,
+        null, null, null, null, null, null);
+
+        List<Map<String, String>> listOfBook = new ArrayList<Map<String, String>>();
+
+        cursor.moveToFirst();
+        while(!cursor.isAfterLast()){
+            Log.d("TEST", database.toString());
+            int id = cursor.getInt(0);
+            String titre = cursor.getString(1);
+            String auteur = cursor.getString(2);
+
+            Map<String, String> bookMap = new HashMap<String, String>();
+            bookMap.put("img", String.valueOf(R.mipmap.ic_launcher)); // use available img
+            bookMap.put("author", auteur);
+            bookMap.put("title", titre);
+            bookMap.put("isbn", Integer.toString(id));
+
+            listOfBook.add(bookMap);
+            cursor.moveToNext();
+        }
+        // Close the cursor
+        cursor.close();
+        Log.d("MAP", listOfBook.toString());
+        return listOfBook;
+    }
+}
